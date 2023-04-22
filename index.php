bc00c9f16487075cffc298c48371f46d864ffeae
reccalyn <reccalynloto@gmail.com>
2023-04-22 11:37:24 +0800

    php login script added

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
+<?php
+//start the session function
+session_start();
+
+//create a pre defined username and password since we do not have database
+$acc_username = "Reccalyn";
+$acc_password = "Reccalyn24";
+$acc_fullname = "Reccalyn Loto";
+$acc_address = "caganhao";
+
+//check the current url for the redirection later
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+//condition to know if the button is clicked
+if (isset($_REQUEST['login_button']) === true) {
+	//get the username and password from the form and compare to the predefined username and password
+	if ($_REQUEST['form_username'] != $acc_username) {
+		header("Location: ".$url_add."?notexist");
+	}
+	else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password']!= $acc_password) {
+		header("Location: ".$url_add."?wrongpass");
+	}
+
+	else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password']== $acc_password) {
+		header("Location: ".$url_add."?success");
+
+		$_SESSION['ses_username']=$acc_username;
+		$_SESSION['ses_password']=$acc_password;
+		$_SESSION['ses_fullname']=$acc_fullname;
+		$_SESSION['ses_address']=$acc_address;
+
+	}
+}
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -24,38 +60,52 @@
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
-						<form action="#" class="login-form">
+				<form method="POST" class="login-form">
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
-		      		</div>
-	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
-	            </div>
-	            <div class="form-group d-md-flex">
-	            	<div class="w-50">
-	            		<label class="checkbox-wrap checkbox-primary">Remember Me
-									  <input type="checkbox" checked>
-									  <span class="checkmark"></span>
-									</label>
-								</div>
-								<div class="w-50 text-md-right">
-									<a href="#">Forgot Password</a>
-								</div>
-	            </div>
-	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
-	            </div>
-	          </form>
-	        </div>
-				</div>
-			</div>
-		</div>
-	</section>
 
-  <!--<script src="js/jquery.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script> -->
+		      			<?php
+		      			if (isset($_REQUEST['notexist'])===true) {
+		      				echo "<div class=alert alert-danger' role='alert'>Username does not exist...</div>";	
+		      				}	else if (isset($_REQUEST['wrongpass'])===true) {
+		      						echo "<div class=alert alert-danger' role='alert'>Incorrect Password...</div>";
+		      				}	else if (isset($_REQUEST['success'])===true) {
+		      						echo "<div class=alert alert-success' role='alert'>Redirecting...</div>";
+		      						header("Refresh: 5; url=account.php");
+		      				}	else if (isset($_REQUEST['logout'])===true) {
+		      						echo "<div class=alert alert-info' role='alert'>Thank You...</div>";
+		      				}	else if (isset($_REQUEST['logfirst'])===true) {
+		      						echo "<div class=alert alert-info' role='alert'>Please Login first.</div>";
+
+		      				}
+		      				    
+		      			?>
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username" required>
+		      		</div>
+	           		<div class="form-group d-flex">
+	              		<input type="password" class="form-control rounded-left" placeholder="Password" name="form_password" required>
+	            	</div>
+	            	<div class="form-group d-md-flex">
+	            		<div class="w-50">
+	            			<label class="checkbox-wrap checkbox-primary">Remember Me
+								<input type="checkbox" checked>
+								<span class="checkmark"></span>
+							</label>
+						</div>
+						<div class="w-50 text-md-right">
+							<a href="#">Forgot Password</a>
+						</div>
+	            	</div>
+	            	<div class="form-group">
+
+	            		<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
+	            	</div>
+	          	</form>
+	        </div>
+		</div>
+	</div>
+	</div>
+	</section>
 
 	</body>
 </html>
